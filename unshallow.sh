df88ffc54f023e1c5451b943703e2e5f34d75489
Victor Chong <victor.chong@linaro.org>
2017-09-13 07:52:30 +0100

    reduce list of repos to unshallow

--- unshallow.sh
+++ unshallow.sh
@@ -3,27 +3,42 @@
 ROOT=${ROOT:-$(pwd)}
 echo "ROOT = ${ROOT}"
 
-echo "unshallow frameworks/base for hikey-n-workarounds"
-cd ${ROOT}/frameworks/base
-git fetch aosp --unshallow
-echo "unshallow android-patchsets"
-cd ${ROOT}/android-patchsets
-git fetch linaro-android --unshallow
+#echo "unshallow frameworks/base for hikey-n-workarounds"
+#cd ${ROOT}/frameworks/base
+#git fetch aosp --unshallow
+
+#echo "unshallow android-patchsets"
+#cd ${ROOT}/android-patchsets
+#git fetch linaro-android --unshallow
+
 echo "unshallow dlh"
 cd ${ROOT}/device/linaro/hikey
-git fetch linaro-android --unshallow
+git fetch github --unshallow
+
 echo "unshallow kernel"
 cd ${ROOT}/kernel/linaro/hisilicon
 git fetch aosp --unshallow
+
 echo "unshallow xtest"
 cd ${ROOT}/external/optee_test
 git fetch github --unshallow
+
 echo "unshallow client"
 cd ${ROOT}/external/optee_client
 git fetch github --unshallow
+
 echo "unshallow optee_examples"
 cd ${ROOT}/external/optee_examples
 git fetch github --unshallow
+
 echo "unshallow optee_os"
 cd ${ROOT}/optee/optee_os
+git fetch github --unshallow
+
+echo "unshallow l-loader"
+cd ${ROOT}/device/linaro/l-loader
+git fetch github --unshallow
+
+echo "unshallow tools-images-hikey960"
+cd ${ROOT}/device/linaro/tools-images-hikey960
 git fetch github --unshallow
